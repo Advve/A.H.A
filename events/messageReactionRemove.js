3e0d5394c3b41d6bd9e7280be9ec715180883411
JJedut <131999253+JJedut@users.noreply.github.com>
2023-06-15 23:26:59 +0200

    Update messageReactionRemove.js

--- events/messageReactionRemove.js
+++ events/messageReactionRemove.js
@@ -1,15 +1,19 @@
 module.exports = {
-	name: 'messageReactionRemove',
-	async execute(reaction, user) {
-		const { message, emoji } = reaction;
-		if (user.bot) return;
+	name: 'messageReactionRemove',// Nazwa zdarzenia, które obsługuje ten kod
+	async execute(reaction, user) {// Asynchroniczna funkcja obsługująca zdarzenie messageReactionRemove
+		const { message, emoji } = reaction;// Pobranie wiadomości i emoji z reakcji
+		if (user.bot) return;// Ignorowanie reakcji od botów
 
+		// Pobranie roli na podstawie ID wiadomości i ustawień
 		const roleId = await message.client.settings.get(message.guildId, `reactionRole.${message.id}`);
+
+		// Pobranie emoji na podstawie ID wiadomości i ustawień
 		const reactionEmoji = await message.client.settings.get(message.guildId, `reactionEmoji.${message.id}`);
 
+		// Jeśli istnieje rola i zarejestrowane emoji odpowiada aktualnej reakcji
 		if (roleId && reactionEmoji === emoji.name) {
-			const member = await message.guild.members.fetch(user.id);
-			await member.roles.remove(roleId);
+			const member = await message.guild.members.fetch(user.id);// Pobranie informacji o członku serwera
+			await member.roles.remove(roleId);// Usunięcie roli od członka
 		}
 	},
 };
