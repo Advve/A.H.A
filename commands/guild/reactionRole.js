28377045573f7df11f7c03e595c34886454ac963
JJedut <131999253+JJedut@users.noreply.github.com>
2023-06-15 18:31:22 +0200

    Update reactionRole.js

--- commands/guild/reactionRole.js
+++ commands/guild/reactionRole.js
@@ -8,15 +8,20 @@ module.exports = {
 		.addRoleOption(option => option.setName('role').setDescription('The role to assign').setRequired(true))
 		.addStringOption(option => option.setName('emoji').setDescription('The emoji to react with').setRequired(true))
 		.addStringOption(option => option.setName('message').setDescription('The message to display').setRequired(true)),
+	
+	// Metoda execute jest wywoływana, gdy komenda jest uruchamiana
 	async execute(interaction) {
+		// Sprawdzenie, czy użytkownik ma uprawnienia do zarządzania rolami
 		if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageRoles)) {
 			return interaction.reply({ content: 'You do not have permission to manage roles!', ephemeral: true });
 		}
 
+		// Pobranie wartości opcji komendy
 		const role = interaction.options.getRole('role');
 		const emoji = interaction.options.getString('emoji');
 		const messageText = interaction.options.getString('message');
 
+		// Wysłanie wiadomości z reakcją do kanału
 		const message = await interaction.channel.send({
 			content: messageText,
 			components: [],
@@ -24,8 +29,10 @@ module.exports = {
 			fetchReply: true,
 		});
 
+		// Dodanie reakcji do wiadomości
 		message.react(emoji);
 
+		// Zapisanie informacji o roli i emoji w bazie danych
 		await interaction.client.settings.set(interaction.guildId, `reactionRole.${message.id}`, role.id);
 		await interaction.client.settings.set(interaction.guildId, `reactionEmoji.${message.id}`, emoji);
 
