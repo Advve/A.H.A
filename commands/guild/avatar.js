bac38d60ed62c3029f9bccd90487d5eff5cfc2c2
JJedut <131999253+JJedut@users.noreply.github.com>
2023-06-15 17:29:34 +0200

    Update avatar.js

--- commands/guild/avatar.js
+++ commands/guild/avatar.js
@@ -14,19 +14,23 @@ module.exports = {
 		const user = interaction.options.getUser('user');
 
 		if (user) {
+			// Tworzy osadzenie (embed) dla docelowego użytkownika
 			const targetembed = new EmbedBuilder()
 				.setTitle(`${user.username} Avatar`)
 				.setImage(user.displayAvatarURL({ size: 1024, dynamic: true }))
 				.setColor('#ffff00');
 
+			// Odpowiada na interakcję, wyświetlając osadzenie (embed) dla docelowego użytkownika
 			interaction.reply({ embeds: [targetembed] });
 		}
 		else {
+			// Tworzy osadzenie (embed) dla użytkownika, który wywołał komendę
 			const interactionembed = new EmbedBuilder()
 				.setTitle(`${interaction.user.username} Avatar`)
 				.setImage(interaction.user.displayAvatarURL({ size: 1024, dynamic: true }))
 				.setColor('#ffff00');
 
+			// Odpowiada na interakcję, wyświetlając osadzenie (embed) dla użytkownika, który wywołał komendę
 			interaction.reply({ embeds: [interactionembed] });
 		}
 	},
