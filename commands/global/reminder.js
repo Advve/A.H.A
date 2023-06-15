f493978b2870e70a8fe3b7e901a7108ad912f685
JJedut <131999253+JJedut@users.noreply.github.com>
2023-06-15 17:11:51 +0200

    Update reminder.js

--- commands/global/reminder.js
+++ commands/global/reminder.js
@@ -15,25 +15,29 @@ module.exports = {
 				.setDescription('The time (in minutes) until the reminder')
 				.setRequired(true)),
 	async execute(interaction = new CommandInteraction()) {
+		// Pobiera wartości argumentów 'time' i 'message' z interakcji
 		const time = interaction.options.getInteger('time');
 		const message = interaction.options.getString('message');
 		const userId = interaction.user.id;
 
+		// Oblicza datę i godzinę zakończenia przypomnienia na podstawie podanego czasu
 		const dueDate = Date.now() + time * 60 * 1000;
 
+		// Wstawia nowe przypomnienie do bazy danych
 		db.run(`
     INSERT INTO reminders (user_id, due_date, message)
     VALUES (?, ?, ?)
 `, [userId, dueDate, message], function(err) {
 			if (err) console.error(err);
-			// ID of the reminder just inserted
+			// ID dodanego właśnie przypomnienia
 			const reminderId = this.lastID;
 
+			// Ustawia opóźniony czas, po którym zostanie wysłane przypomnienie
 			setTimeout(async () => {
 				const user = await interaction.client.users.fetch(userId);
 				user.send(`🔔 Reminder: ${message}`);
 
-				// Delete the reminder from the database
+				// Usuwa przypomnienie z bazy danych
 				db.run(`
             DELETE FROM reminders WHERE id = ?
         `, [reminderId], (err) => {
@@ -42,6 +46,7 @@ module.exports = {
 			}, time * 60 * 1000);
 		});
 
+		// Odpowiada na interakcję potwierdzając ustawienie przypomnienia
 		await interaction.reply(`⏰ Okay, I will remind you in ${time} minute(s): ${message}`);
 	},
 };
